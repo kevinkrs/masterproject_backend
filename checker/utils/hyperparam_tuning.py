fdde5559d4006ab201ac65c962de4a4bcdca54c9
kevinkrs7 <kraus.kevin@proton.me>
2022-09-26 11:41:13 +0200

    ray hot fix

--- checker/utils/hyperparam_tuning.py
+++ checker/utils/hyperparam_tuning.py
@@ -13,23 +13,22 @@ class HyperParamTuning:
         self.model = model
         self.dm = datamodule
 
-    def train_ray(self, data_dir=None, num_epochs=10, num_gpus=1):
-        metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
-        trainer = Trainer(
-            max_epochs=num_epochs,
-            gpus=num_gpus,
-            progress_bar_refresh_rate=0,
-            callbacks=[TuneReportCallback(metrics, on="validation_end")],
-        )
-        trainer.fit(self.model, self.dm)
-
     def run(self):
+        def train_ray(self, data_dir=None, num_epochs=10, num_gpus=1):
+            metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
+            trainer = Trainer(
+                max_epochs=num_epochs,
+                gpus=num_gpus,
+                progress_bar_refresh_rate=0,
+                callbacks=[TuneReportCallback(metrics, on="validation_end")],
+            )
+            trainer.fit(self.model, self.dm)
+
         num_samples = 10
         num_epochs = 10
         gpus_per_trial = 2
 
         data = os.path.join(self.base_dir, self.config["full_data_path"])
-        # Download data
 
         config = {
             "layer_1": tune.choice([32, 64, 128]),
@@ -39,7 +38,7 @@ class HyperParamTuning:
         }
 
         trainable = tune.with_parameters(
-            self.train_ray,
+            train_ray,
             data_dir=data,
             num_epochs=num_epochs,
             num_gpus=gpus_per_trial,

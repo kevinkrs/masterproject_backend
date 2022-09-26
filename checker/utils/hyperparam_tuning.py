e45c10301dfa7f485382333d0de8152a60c0d784
Phillip Rath <65727386+PhillipRt@users.noreply.github.com>
2022-09-26 17:04:20 +0200

    edit cpu and gpu num

--- checker/utils/hyperparam_tuning.py
+++ checker/utils/hyperparam_tuning.py
@@ -85,7 +85,7 @@ class HyperParamTuning:
         mode="min",
         config=config,
         num_samples=num_samples,
-        resources_per_trial=get_tune_resources(num_workers=3, use_gpu=True),
+        resources_per_trial=get_tune_resources(num_workers=1,num_cpus_per_worker=3, use_gpu=True),
 
         name="tune_bert")
         

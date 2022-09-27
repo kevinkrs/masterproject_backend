58e26891e95477554c7e944d2e797ce4ec758950
Phillip Rath <65727386+PhillipRt@users.noreply.github.com>
2022-09-27 14:16:19 +0200

    fix validation

--- checker/utils/hyperparam_tuning.py
+++ checker/utils/hyperparam_tuning.py
@@ -16,14 +16,14 @@ class HyperParamTuning:
 
     def run(self):
         def train_ray(data_dir=None, num_epochs=10, num_gpus=1):
-            metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
+            metrics = {"loss": "avg_val_loss"}
             callbacks = [TuneReportCallback(metrics, on="validation_end")]
             trainer = pl.Trainer(
                 max_epochs=num_epochs,
                 callbacks=callbacks,
-                #devices=num_gpus, accelerator="auto",
+                # devices=num_gpus, accelerator="auto",
                 strategy=RayStrategy(num_workers=1, use_gpu=True))
-          
+
             model = TransformerModel(self.config).model
             trainer.fit(model, self.dm)
 
@@ -74,20 +74,20 @@ class HyperParamTuning:
           ),
           param_space=config
         )
-         
+
         results = tuner.fit()
         analysis = results.get_best_result().config
         """
 
         analysis = tune.run(
-        train_ray,
-        metric="loss",
-        mode="min",
-        config=config,
-        num_samples=num_samples,
-        resources_per_trial=get_tune_resources(num_workers=1,num_cpus_per_worker=3, use_gpu=True),
+            train_ray,
+            metric="loss",
+            mode="min",
+            config=config,
+            num_samples=num_samples,
+            resources_per_trial=get_tune_resources(num_workers=1, num_cpus_per_worker=3, use_gpu=True),
 
-        name="tune_bert")
-        
+            name="tune_bert")
+
         print("Best hyperparameters found were: ", analysis.best_config)
         return analysis

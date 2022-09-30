cb65006ea319e75193a880d95fc51252c753d453
kevinkrs7 <kraus.kevin@proton.me>
2022-09-30 21:00:38 +0200

    fixed double saved_models folders required for model loading bug

--- checker/train.py
+++ checker/train.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     mlflow.set_experiment(config["model"])
 
-    model_output_path = os.path.join(base_dir, "checker", config["model_output_path"])
+    model_output_path = os.path.join(base_dir, config["model_output_path"])
 
     os.makedirs(os.path.join(base_dir, config["model_output_path"]), exist_ok=True)
 
